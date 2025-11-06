cdb9deb6755955520003189e30466eaf076687a9
ThakurAashish <officialashishthakur@gmail.com>
2025-11-07 04:16:38 +0530

    Add files via upload

---
+++ shorthand.py
@@ -0,0 +1,10 @@
+# If...else in one statement/line 
+# (shorthand If else statement)
+
+a = 3040
+b = 3040
+
+print("A") if (a>b) else print("=") if a==b else print("B")
+
+c = print(9) if  a>b else 0
+print(c)
